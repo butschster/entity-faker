42d1a4700d5b1fcf01f669ab2f1f21cb39c0ca30
butschster <butschster@gmail.com>
2021-07-21 23:38:17 +0300

    Added abstract entity factory

--- src/LaminasEntityFactory.php
+++ src/LaminasEntityFactory.php
@@ -6,11 +6,8 @@ namespace Butschster\EntityFaker;
 use Laminas\Hydrator\HydratorInterface;
 use ReflectionClass;
 
-class LaminasEntityFactory implements EntityFactoryInterface
+class LaminasEntityFactory extends AbstractEntityFactory
 {
-    private array $afterCreation = [];
-    private array $beforeCreation = [];
-
     private HydratorInterface $hydrator;
 
     public function __construct(HydratorInterface $hydrator)
@@ -33,33 +30,5 @@ class LaminasEntityFactory implements EntityFactoryInterface
     public function hydrate(object $entity, array $data): object
     {
         return $this->hydrator->hydrate($data, $entity);
-    }
-
-    /**
-     * Add a callback to run before creating an entity or array of entities.
-     * @param callable $callback
-     */
-    public function beforeCreation(callable $callback): void
-    {
-        $this->beforeCreation[] = $callback;
-    }
-
-    public function beforeCreationCallbacks(): array
-    {
-        return $this->beforeCreation;
-    }
-
-    /**
-     * Add a callback to run after creating an entity or array of entities.
-     * @param callable $callback
-     */
-    public function afterCreation(callable $callback): void
-    {
-        $this->afterCreation[] = $callback;
-    }
-
-    public function afterCreationCallbacks(): array
-    {
-        return $this->afterCreation;
     }
 }
